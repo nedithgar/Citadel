1b8800d0b4834225c540fad9e4181b7260862a47
Nedithgar Amirka <150447520+nedithgar@users.noreply.github.com>
2025-08-01 22:19:03 +0800

    build: remove TestCertificates resource reference from Package.swift
    Remove the TestCertificates resource copy declaration as test certificates
    and associated scripts have been removed from the project.

--- Package.swift
+++ Package.swift
@@ -48,9 +48,6 @@ let package = Package(
                 .product(name: "NIOSSH", package: "Joannis-swift-nio-ssh"),
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "Logging", package: "swift-log"),
-            ],
-            resources: [
-                .copy("TestCertificates")
             ]
         ),
     ]
